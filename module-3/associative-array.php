9830b07e01e56a22cdb9103fd782607aaa2e20d0
H M Mohiuddin <mohiuddin3824@gmail.com>
2023-09-22 12:47:36 +0600

    Associative Array

---
+++ module-3/associative-array.php
@@ -0,0 +1,14 @@
+<?php
+
+$students = [
+    "jamila" => 'Age: 24, address: chittagong, mobile: 01831332732',
+    "jamal" => 'Age: 24, address: Dhaka, Mobile: 01911555084',
+    "kamal" => 'Age: 24, address: Dhaka, Mobile: 01911555084',
+    "hasan" => 'Age: 24, address: Dhaka, Mobile: 01911555084',
+];
+
+//echo $students['jamal'] . "\n";
+
+foreach ($students as $key => $value) {
+    echo $key ." => ". $value . "\n";
+}// foreach loop is dedicated for associative array
